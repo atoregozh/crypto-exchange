5def3aab80bee00e664b84bb780cd2b3765e54bb
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2021-01-01 18:07:08 -0500

    Refactor code to improve readability

--- calc_rate.py
+++ calc_rate.py
@@ -2,25 +2,29 @@ from binance import calc_binance_withdrawals
 from luno import calc_luno_withdrawals
 import json
 
-dollar_amt = 1000
-
 
 def exchange(from_amount, from_currency, to_currency):
+    # Binance Converstion to Coin
     binance_withdrawal_list = calc_binance_withdrawals(from_amount)
-    luno_withdrawal_input = {
-        '{}{}'.format(x['to_symbol'], to_currency) :
-            {
-                'from_symbol': x['to_symbol'],
-                'from_name': x['to_name'],
-                'from_amount': x['to_amount'],
-                'to_symbol': to_currency,
-            } for x in binance_withdrawal_list
+
+    # Luno Converstion to destination currency
+    luno_withdrawal_input = {}
+    for binance_withdrawal in binance_withdrawal_list:
+        currency_pair = '{}{}'.format(binance_withdrawal['to_symbol'], to_currency)
+        input_dict = {
+            'from_symbol': binance_withdrawal['to_symbol'],
+            'from_name': binance_withdrawal['to_name'],
+            'from_amount': binance_withdrawal['to_amount'],
+            'to_symbol': to_currency,
         }
+        luno_withdrawal_input.update({currency_pair: input_dict})
 
     luno_withdrawal_list = calc_luno_withdrawals(luno_withdrawal_input)
 
-    exchange_result = [
-        {
+    # Format and sort result
+    overall_exchange_result = []
+    for luno_result in luno_withdrawal_list:
+        overall_exchange_result.append({
             'from_currency': from_currency,
             'from_amount': from_amount,
             'coin_symbol': luno_result['from_symbol'],
@@ -30,30 +34,14 @@ def exchange(from_amount, from_currency, to_currency):
             'to_currency': luno_result['to_symbol'],
             'to_amount': luno_result['to_amount'],
             'exchange_rate': luno_result['to_amount'] / from_amount
-        }
-        for luno_result in luno_withdrawal_list
-    ]
+        })
 
-    return sorted(exchange_result, key = lambda x: x['exchange_rate'], reverse=True)
-
-
-def calc_rate():
-    result = exchange(dollar_amt, 'USD', 'NGN')
-    for single_exchange in result:
-        print(json.dumps(single_exchange, indent=4, sort_keys=True))
+    sorted_exchange_result = sorted(overall_exchange_result, key = lambda x: x['exchange_rate'], reverse=True)
+    return sorted_exchange_result
 
 
 if __name__ == '__main__':
-    calc_rate();
-
-
-class ExchangeResult:
-    def __init__(self, from_amount, from_currency, to_amount, to_currency, intermediate_coin, breakdown):
-        self.from_amount = from_amount
-        self.from_currency = from_currency
-        self.to_amount = to_amount
-        self.to_currency = to_currency
-        self.intermediate_coin = intermediate_coin
-        self.breakdown = breakdown
-
+    result_list = exchange(1000, 'USD', 'NGN')
+    for single_exchange in result_list:
+        print(json.dumps(single_exchange, indent=4, sort_keys=True))
 
--- luno.py
+++ luno.py
@@ -21,20 +21,29 @@ def get_luno_client():
 
 def calc_luno_withdrawals(request_map):
     """
-        :param request_map: A dict which maps strings to dict objects of format { currency_pair: { from_symbol: string, 'from_name': string, 'from_amount': int, 'to_symbol': string } }
+        :param request_map: A dict which maps strings to dict objects of format
+        {
+            currency_pair: {
+                from_symbol: string,
+                'from_name': string,
+                'from_amount': int,
+                'to_symbol': string
+            }
+        }
         :return: list of dict objects with keys: from_symbol, from_name, from_amount, to_symbol, to_amount
         """
 
-    luno_to_binance_currency_pairs = {
-        '{}{}'.format(
+    luno_to_binance_currency_pairs = {}
+    for key, val in request_map.items():
+        binance_pair = key
+        luno_pair = '{}{}'.format(
             binance_to_luno_symbol.get(val['from_symbol'], val['from_symbol']),
             binance_to_luno_symbol.get(val['to_symbol'], val['to_symbol']))
-        : key
-        for key, val in request_map.items()
-    }
+        luno_to_binance_currency_pairs.update({luno_pair : binance_pair})
 
     luno_client = get_luno_client()
     luno_tickers = luno_client.get_tickers()
+
     results = []
     for ticker in luno_tickers['tickers']:
         luno_currency_pair = ticker['pair']
@@ -42,8 +51,11 @@ def calc_luno_withdrawals(request_map):
             continue
 
         time.sleep(0.5)  # To account for trading fee API Rate limit
-        fee_response = luno_client.get_fee_info(luno_currency_pair)
-        trading_fee = max(float(fee_response['maker_fee']), float(fee_response['taker_fee']))
+
+        trading_fee_response = luno_client.get_fee_info(luno_currency_pair)
+        # Set trading fee as the max between the maker_fee and taker_fee
+        trading_fee = max(float(trading_fee_response['maker_fee']),
+                          float(trading_fee_response['taker_fee']))
 
         request_currency_pair = luno_to_binance_currency_pairs[luno_currency_pair]
         request_object = request_map[request_currency_pair]
@@ -51,16 +63,15 @@ def calc_luno_withdrawals(request_map):
         from_amount = request_object['from_amount']
         from_amount_minus_trading_fee = from_amount - (from_amount * trading_fee)
 
-        to_amount = from_amount_minus_trading_fee * float(ticker['last_trade'])
-
-        to_amount_minus_withdrawal_fee = to_amount - luno_withdrawal_fee_ngn
+        exchange_rate = float(ticker['last_trade'])
+        to_amount = (from_amount_minus_trading_fee * exchange_rate) - luno_withdrawal_fee_ngn
 
         results.append({
             'from_symbol': request_object['from_symbol'],
             'from_name': request_object['from_name'],
             'from_amount': request_object['from_amount'],
             'to_symbol': request_object['to_symbol'],
-            'to_amount': to_amount_minus_withdrawal_fee,
+            'to_amount': to_amount,
             'trading_fee': trading_fee,
             'withdrawal_fee': luno_withdrawal_fee_ngn,
         })
@@ -68,7 +79,7 @@ def calc_luno_withdrawals(request_map):
     return results
 
 
-def run_luno():
+def print_luno_api_responses():
     luno_client = get_luno_client()
     try:
         res = luno_client.get_ticker(pair='XBTZAR')
@@ -87,4 +98,4 @@ def run_luno():
 
 
 if __name__ == '__main__':
-    run_luno()
+    print_luno_api_responses()
